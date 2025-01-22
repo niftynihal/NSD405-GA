94583b92cbe5e8b906905268a72f79a59128a49c
Nihal <niftynihal@gmail.com>
2025-01-22 20:50:56 +0530

    GA - firstjob

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 let str = "hello world";
 console.log(str)
 
-
-
-let str2 = "hello world";
-console.log(str2)
-
-
-
-let str3 = "hello world";
-console.log(str3)
-
-
-
-let str4 = "hello world";
-console.log(str4)
-
 let a = 1
 let b = 5
 if(a<b){
